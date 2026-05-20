wyre-technology/sherweb-mcp

feat(registry): publish to MCP Registry on release

Dockerfile
@@ -25,3 +25,4 @@ LABEL description="Sherweb Partner API MCP Server"
 LABEL org.opencontainers.image.title="sherweb-mcp"
 LABEL org.opencontainers.image.description="Model Context Protocol server for Sherweb Partner API - distributor billing, service providers, subscriptions"
 LABEL org.opencontainers.image.vendor="Wyre Technology"
+LABEL io.modelcontextprotocol.server.name="io.github.wyre-technology/sherweb-mcp"

WYRE-AI/mimecast-mcp

Dockerfile
@@ -24,4 +24,5 @@ ENV NODE_ENV=production \
     MCP_HTTP_PORT=8080 \
     AUTH_MODE=gateway \
     LOG_LEVEL=info
+LABEL io.modelcontextprotocol.server.name="io.github.wyre-technology/mimecast-mcp"
 CMD ["node", "dist/index.js"]